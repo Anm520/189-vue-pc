05f6649eb060cf4610087ad24ea2c241586a38f8
Anm520 <944606394@qq.com>
2025-03-20 21:26:06 +0800

    优化

--- src/stores/config.js
+++ src/stores/config.js
@@ -2,9 +2,9 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useConfigStore = defineStore('config', () => {
-  const chanel_url = ref('http://141.147.11.144:9528')
-  function setChangelUrl(url) {
-    chanel_url.value = url
-  }
-  return { chanel_url, setChangelUrl }
+    const chanel_url = ref('')
+    function setChangelUrl(url) {
+        chanel_url.value = url
+    }
+    return { chanel_url, setChangelUrl }
 })
