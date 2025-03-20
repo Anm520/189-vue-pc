b516f0a8559afccedc1708beba8d8f0f7deec475
Anm520 <944606394@qq.com>
2025-03-20 21:30:01 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -7,33 +7,36 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    build: {
+        outDir: 'cloud-vue-pc', // 将这里的 'your-desired-directory-name' 替换为你想要的目录名
     },
-  },
+    plugins: [vue(), vueJsx(), VueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
 
-  server: {
-    port: 8080,
-    // host: '0.0.0.0', // 允许外部访问，如果你需要在同一局域网下的其他设备访问此开发服务器，可以设置此选项
-    open: true,
-    proxy: {
-      '/proxy': {
-        target: 'https://cloud.189.cn',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/proxy/, ''),
-      },
-      '/locality': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/locality/, ''),
-      },
-      '/cloudApi': {
-        target: 'https://api.cloud.189.cn',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cloudApi/, ''),
-      },
+    server: {
+        port: 8080,
+        // host: '0.0.0.0', // 允许外部访问，如果你需要在同一局域网下的其他设备访问此开发服务器，可以设置此选项
+        open: true,
+        proxy: {
+            '/proxy': {
+                target: 'https://cloud.189.cn',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/proxy/, ''),
+            },
+            '/locality': {
+                target: 'http://127.0.0.1:3000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/locality/, ''),
+            },
+            '/cloudApi': {
+                target: 'https://api.cloud.189.cn',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/cloudApi/, ''),
+            },
+        },
     },
-  },
 })
