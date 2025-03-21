687bad476e779430338ef3f0f158347a8091b856
Anm520 <944606394@qq.com>
2025-03-21 23:11:42 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,7 @@ const menuRoutes = [
     //   },
 ]
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
